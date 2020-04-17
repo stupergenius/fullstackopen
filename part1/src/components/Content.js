4de3a0c713b3a7bbf7676710cc268acf46cc6d0a
Benjamin Snider <stupergenius@gmail.com>
2020-04-17 18:32:43 -0400

    Changes for parts 1.1-1.2

---
+++ part1/src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Part from './Part'
+
+export default ({ part1, exercises1, part2, exercises2, part3, exercises3, }) => {
+  return (
+    <div>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </div>
+  )
+}
---
+++ part1/src/components/Total.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default ({exercises}) => <p>Number of exercises {exercises}</p>
