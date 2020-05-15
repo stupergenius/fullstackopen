bc1df2f53c4f753603c4a33b5934f3c4c2ba0f10
Benjamin Snider <stupergenius@gmail.com>
2020-05-15 14:35:41 -0400

    Part 1.8 - refactor statistics.

--- part1.6/src/App.js
+++ part1.6/src/App.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({good, neutral, bad}) => {
   const all = () => good + neutral + bad
   const average = () => {
     const votes = all()
@@ -23,25 +19,38 @@ const App = () => {
 
     return (good / votes) * 100.0
   }
+  
+  return (
+    <>
+      <h1>statistics</h1>
+
+      <p>
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad} <br />
+        all {all()} <br />
+        average {average()} <br />
+        positive {percentGood()} %<br />
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <>
       <h1>give feedback</h1>
 
-      <button onClick={() => setGood(good+1)}>good</button>
-      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setBad(bad+1)}>bad</button>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h1>statistics</h1>
-
-      <p>
-        good {good} <br/>
-        neutral {neutral} <br/>
-        bad {bad} <br/>
-        all {all()} <br/>
-        average {average()} <br/>
-        positive {percentGood()} %<br/>
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
