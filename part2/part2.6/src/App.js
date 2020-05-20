8d1d043d593e5f3e7a44e6acf7baa032fa1f2cbf
Benjamin Snider <stupergenius@gmail.com>
2020-05-20 16:11:07 -0400

    Part 2.6 - phonebook - adding people

---
+++ part2/part2.6/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const Person = ({person}) => {
+  return (
+    <>
+      <span>{person.name}</span>
+      <br />
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = ({ target }) => {
+    setNewName(target.value)
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const newPerson = { name: newName }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleFormSubmit}>add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      {persons.map(p =>
+        <Person key={p.name} person={p} />
+      )}
+    </div>
+  )
+}
+
+export default App
