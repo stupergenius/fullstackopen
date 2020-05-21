d7abc32d71624db38780e634269699ec32379cd5
Benjamin Snider <stupergenius@gmail.com>
2020-05-21 09:45:03 -0400

    Part 2.11 - using api to get phonebook entries

--- part2/part2.6/src/App.js
+++ part2/part2.6/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 const Persons = ({ persons }) => persons.map(p =>
@@ -40,15 +41,18 @@ const PersonForm = ({ newName, newNumber, onNameChange, onNumberChange, onSubmit
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const personsToShow = searchQuery.length > 0
     ? persons.filter(p => p.name.toLocaleLowerCase().includes(searchQuery))
