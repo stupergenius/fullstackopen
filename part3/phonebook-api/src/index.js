6b0104abbb84a05c7d31aeac707e7c0f8dd3fec2
Benjamin Snider <stupergenius@gmail.com>
2020-05-29 15:47:18 -0400

    Adding unknown endpoint support.

--- part3/phonebook-api/src/index.js
+++ part3/phonebook-api/src/index.js
@@ -13,6 +13,11 @@ app.get('/info', (req, res) => {
 
 app.use('/api/persons', personsRouter)
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
