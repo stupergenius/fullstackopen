8338f70ce28fe1b6a1e11fe798a9826d754f1b5f
Benjamin Snider <stupergenius@gmail.com>
2020-05-29 16:38:25 -0400

    Part 3.9 - Using node backend for phonebook

---
+++ part3/phonebook-web/src/services/persons.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const read = id => {
+  const request = axios.get(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const _delete = id => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
+export default { getAll, read, create, update, delete: _delete }
