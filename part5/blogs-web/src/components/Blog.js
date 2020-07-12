3ffab855992d7efc86cf7f13cad20604df5db65f
Benjamin Snider <stupergenius@gmail.com>
2020-07-11 20:14:26 -0400

    Part 5.7 - Toggling blogs between overview and detail modes. Add non-functional like button.

--- part5/blogs-web/src/components/Blog.js
+++ part5/blogs-web/src/components/Blog.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
+const blogStyle = {
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+}
+
+const FullBlog = ({ blog, onHide }) => (
+  <p>
+    {blog.title} <button type="button" onClick={onHide}>hide</button> <br />
+    {blog.url} <br />
+    likes {blog.likes} <button type="button">like</button> <br />
+    {blog.author}
+  </p>
 )
+
+const SimpleBlog = ({ blog, onView }) => (
+  <p>
+    {blog.title} {blog.author} <button type="button" onClick={onView}>view</button>
+  </p>
+)
+
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      {isExpanded
+        ? <FullBlog blog={blog} onHide={() => setIsExpanded(false)} />
+        : <SimpleBlog blog={blog} onView={() => setIsExpanded(true)} />}
+    </div>
+  )
+}
 
 export default Blog
