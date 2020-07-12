d36cd6cc944bbff7dc23a73fd430b7a3882dd185
Benjamin Snider <stupergenius@gmail.com>
2020-07-12 09:49:52 -0400

    Part 5.15 - Adding test for like blog button.

--- part5/blogs-web/src/components/Blog.test.js
+++ part5/blogs-web/src/components/Blog.test.js
@@ -36,11 +36,33 @@ test('it shows more details when clicking the view button', () => {
     />,
   )
 
-  const viewButton = component.getByText('view')
+  const viewButton = component.getByRole('button', { name: 'view' })
   fireEvent.click(viewButton)
 
   expect(component.container).toHaveTextContent(blog.title)
   expect(component.container).toHaveTextContent(blog.author)
   expect(component.container).toHaveTextContent(blog.url)
   expect(component.container).toHaveTextContent(`likes ${blog.likes}`)
+})
+
+test('it emits like events', () => {
+  const likeMock = jest.fn()
+
+  const component = render(
+    <Blog
+      blog={blog}
+      onLike={likeMock}
+      onRemove={jest.fn()}
+      showRemove={false}
+    />,
+  )
+
+  const viewButton = component.getByRole('button', { name: 'view' })
+  fireEvent.click(viewButton)
+
+  const likeButton = component.getByRole('button', { name: 'like' })
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(likeMock).toHaveBeenCalledTimes(2)
 })
