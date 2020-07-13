6c7a59a03720806315c35be72569c7a571e89cf7
Benjamin Snider <stupergenius@gmail.com>
2020-07-13 16:17:10 -0400

    Part 5.18 - Adding test for login success and failure.

--- part5/blogs-web/cypress/integration/login.spec.js
+++ part5/blogs-web/cypress/integration/login.spec.js
@@ -1,6 +1,11 @@
 describe('Blog app', () => {
   beforeEach(() => {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      username: 'ben',
+      password: 'asdf',
+      name: 'Ben S',
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +13,23 @@ describe('Blog app', () => {
     cy.contains('log in to application')
     cy.get('input[name="Username"]')
     cy.get('input[name="Password"]')
+  })
+
+  describe('Login', () => {
+    it('succeeds with correct credentials', () => {
+      cy.get('input[name="Username"]').type('ben')
+      cy.get('input[name="Password"]').type('asdf')
+      cy.get('form').contains('login').click()
+
+      cy.contains(/ben s logged in/i)
+    })
+
+    it('fails with wrong credentials', () => {
+      cy.get('input[name="Username"]').type('ben')
+      cy.get('input[name="Password"]').type('the wrong password')
+      cy.get('form').contains('login').click()
+
+      cy.contains(/wrong credentials/i)
+    })
   })
 })
