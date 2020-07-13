538399fb815c099f638b8fb0f9c7dce0054239d8
Benjamin Snider <stupergenius@gmail.com>
2020-07-13 16:29:57 -0400

    Part 5.19 - Adding cypress test for creating a new blog.

--- part5/blogs-web/cypress/integration/blogs_app.spec.js
+++ part5/blogs-web/cypress/integration/blogs_app.spec.js
@@ -32,4 +32,28 @@ describe('Blog app', () => {
       cy.contains(/wrong credentials/i)
     })
   })
+
+  describe('When logged in', () => {
+    beforeEach(() => {
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: 'ben', password: 'asdf',
+      }).then((response) => {
+        localStorage.setItem('user', JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')
+      })
+    })
+
+    it('A blog can be created', () => {
+      cy.contains('new note').click()
+
+      cy.get('input[name="blog_title"]').type('My New Blog')
+      cy.get('input[name="blog_author"]').type('BenS')
+      cy.get('input[name="blog_url"]').type('http://blog.example.com')
+
+      cy.get('button:contains("create")').click()
+
+      cy.contains('My New Blog')
+      cy.contains('BenS')
+    })
+  })
 })
