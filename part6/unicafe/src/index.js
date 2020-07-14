452a8edfdc4145761504bcd43fdd9da907039fcc
Benjamin Snider <stupergenius@gmail.com>
2020-07-14 15:27:57 -0400

    Part 6.2 - Implementing unicafe UI with redux store.

--- part6/unicafe/src/index.js
+++ part6/unicafe/src/index.js
@@ -6,21 +6,21 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const handleAction = (action) => () => {
     store.dispatch({
-      type: 'GOOD'
+      type: action
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>neutral</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={handleAction('GOOD')}>good</button>
+      <button onClick={handleAction('OK')}>neutral</button>
+      <button onClick={handleAction('BAD')}>bad</button>
+      <button onClick={handleAction('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
