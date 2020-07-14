67148ac94531bc667f770255d397c4c069e68c86
Benjamin Snider <stupergenius@gmail.com>
2020-07-14 16:27:58 -0400

    Part 6.8 - Moving note list into its own component - simplifying App component.

--- part6/anecdotes/src/App.js
+++ part6/anecdotes/src/App.js
@@ -1,30 +1,11 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
-import { voteActionFor } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state.sort((a, b) => (a.votes > b.votes ? -1 : 1)))
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(voteActionFor(id))
-  }
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
---
+++ part6/anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteActionFor } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state.sort((a, b) => (a.votes > b.votes ? -1 : 1)))
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteActionFor(id))
+  }
+
+  return (
+    <>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default AnecdoteList
