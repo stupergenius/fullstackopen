779ce0e30ad884994c2bb902b779140061f120db
Benjamin Snider <stupergenius@gmail.com>
2020-07-22 15:28:40 -0400

    Part 6.13 - Using json-server to serve anecdotes to the frontend.

--- part6/anecdotes/src/App.js
+++ part6/anecdotes/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import anecdoteService from './services/anecdotes'
+import { initAnecdotesAction } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => dispatch(initAnecdotesAction(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <AnecdoteList />
