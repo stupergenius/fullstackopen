bfe9f4d8c4b7b50def58709ba30ffc41ba55797e
Benjamin Snider <stupergenius@gmail.com>
2020-07-23 09:40:10 -0400

    Refactor anecdotelist to use mapstate and mapdispatch more idiomatically.

--- part6/anecdotes/src/components/AnecdoteList.js
+++ part6/anecdotes/src/components/AnecdoteList.js
@@ -1,25 +1,14 @@
 import React from 'react'
-import { connect, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { voteAction } from '../reducers/anecdoteReducer'
 import { showNotificationAction } from '../reducers/notificationReducer'
 import Notification from './Notification'
 import Filter from './Filter'
 
 const AnecdoteList = (props) => {
-  const filter = props.filter.trim().toLowerCase()
-  const filterAnecdotes = (allAnecdotes) => {
-    return filter.length === 0
-      ? allAnecdotes
-      : allAnecdotes.filter(a => a.content.toLowerCase().includes(filter))
-  }
-  const sortAnecdotes = toSort => toSort.sort((a, b) => (a.votes > b.votes ? -1 : 1))
-
-  const anecdotes = sortAnecdotes(filterAnecdotes(props.anecdotes))
-  const dispatch = useDispatch()
-
   const vote = (anecdote) => {
-    dispatch(voteAction(anecdote))
-    dispatch(showNotificationAction(`you voted '${anecdote.content}'`, 5))
+    props.voteAction(anecdote)
+    props.showNotificationAction(`you voted '${anecdote.content}'`, 5)
   }
 
   return (
@@ -28,7 +17,7 @@ const AnecdoteList = (props) => {
       <Notification />
       <Filter />
 
-      {anecdotes.map(anecdote =>
+      {props.anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
@@ -44,12 +33,21 @@ const AnecdoteList = (props) => {
 }
 
 const mapStateToProps = (state) => {
-  return {
-    anecdotes: state.anecdotes,
-    filter: state.filter
-  }
+  const filter = state.filter.trim().toLowerCase()
+
+  const filteredAnecdotes = filter.length === 0
+    ? state.anecdotes
+    : state.anecdotes.filter(a => a.content.toLowerCase().includes(filter))
+  const anecdotes = filteredAnecdotes.sort((a, b) => (a.votes > b.votes ? -1 : 1))
+
+  return { anecdotes }
 }
 
-const ConnectedAnecdoteList = connect(mapStateToProps)(AnecdoteList)
+const mapDispatchToProps = {
+  voteAction,
+  showNotificationAction,
+}
+
+const ConnectedAnecdoteList = connect(mapStateToProps, mapDispatchToProps)(AnecdoteList)
 
 export default ConnectedAnecdoteList
