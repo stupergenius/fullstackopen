70f4e58c21c65192ff8e4a9cfe2015bb5930cc97
Benjamin Snider <stupergenius@gmail.com>
2020-08-07 11:31:58 -0400

    Part 9.9 - Adding patientor frontend and ensuring it works with backend.

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -2,11 +2,15 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_, res) => {
+app.get('/api/ping', (_, res) => {
   console.log('someone pinged here');
   res.send('pong');
+});
+
+app.get('/api/patients', (_, res) => {
+  res.send([]);
 });
 
 app.listen(PORT, () => {
